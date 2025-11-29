938373d8b03c98e5a7c6f7fe141af8f3707d9ed9
aymankaddioui <aymankaddioui@gmail.com>
2025-11-29 03:02:36 +0100

    Refactor ESLint configuration to use array format and improve TypeScript support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,22 @@
-export default {
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
   },
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'warn',
-  },
-};
+];
